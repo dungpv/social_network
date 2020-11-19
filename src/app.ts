0ed82fc960ffa8253cfb5f821c6ef99198415816
vietdung0607 <vietdungst@gmail.com>
2020-11-19 19:47:07 +0700

    connect to MongoDB atlas

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import { Route } from 'core/interfaces';
 import express from 'express';
-
+import mongoose from 'mongoose';
 class App{
     public app: express.Application;
     public port: string | number;
@@ -9,6 +9,7 @@ class App{
         this.app = express();
         this.port = process.env.PORT || 5000;
         this.initializerRoutes(routes);
+        this.connectToDatabase();
     }
     public listen(){
         this.app.listen(this.port, () => {
@@ -19,6 +20,22 @@ class App{
         routes.forEach((route) => {
             this.app.use('/', route.router);
         });
+    }
+
+    private connectToDatabase(){
+        try {
+            const connectString = 'mongodb+srv://dungpv:123456a@@master.jw6k2.mongodb.net/dungpv_social?retryWrites=true&w=majority';
+            mongoose.connect(connectString, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useFindAndModify: false,
+                useCreateIndex: true
+            });
+            console.log('Database connected ...');            
+        } catch (error) {
+            console.log('Connect to database error');
+        }
+
     }
 
 }

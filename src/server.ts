51bfe6439f2cc4a4ef716b34a288bb72cb05fcd2
vietdung0607 <vietdungst@gmail.com>
2020-12-11 22:18:24 +0700

    add profile api

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,21 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 import App from "./app";
 import { IndexRoute } from "@modules/index";
-import { validateEnv } from '@core/utils';
-import UserRoute from '@modules/users/user.route';
-import AuthRoute from '@modules/auth/auth.route';
+import { validateEnv } from "@core/utils";
+import UserRoute from "@modules/users/user.route";
+import AuthRoute from "@modules/auth/auth.route";
+import ProfileRoute from "@modules/profile/profile.route";
 
-dotenv.config({ path: `${__dirname}/.env` })
+dotenv.config({ path: `${__dirname}/.env` });
 
 validateEnv();
 
 const routes = [
-    new IndexRoute(),
-    new UserRoute(),
-    new AuthRoute(),
+  new IndexRoute(),
+  new UserRoute(),
+  new AuthRoute(),
+  new ProfileRoute(),
 ];
 
 const app = new App(routes);

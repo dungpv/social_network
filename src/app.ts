d9c6a9666cde4fd9b962907bd5fab281c64c9a34
vietdung0607 <vietdungst@gmail.com>
2020-12-31 16:05:17 +0700

    Add swagger

--- src/app.ts
+++ src/app.ts
@@ -1,75 +1,83 @@
-import { Logger } from '@core/utils';
-import { Route } from '@core/interfaces';
-import cors from 'cors';
-import { errorMiddleware } from '@core/middleware';
-import express from 'express';
-import helmet from 'helmet';
-import hpp from 'hpp';
-import mongoose from 'mongoose';
-import morgan from 'morgan';
+import { Logger } from "@core/utils";
+import { Route } from "@core/interfaces";
+import cors from "cors";
+import { errorMiddleware } from "@core/middleware";
+import express from "express";
+import helmet from "helmet";
+import hpp from "hpp";
+import mongoose from "mongoose";
+import morgan from "morgan";
+import YAML from "yamljs";
+import swaggerUi from "swagger-ui-express";
 
-class App{
-    public app: express.Application;
-    public port: string | number;
-    public production : boolean;
+class App {
+  public app: express.Application;
+  public port: string | number;
+  public production: boolean;
 
-    constructor(routes: Route[]){
-        this.app = express();
-        this.port = process.env.PORT || 5000;
-        this.production = process.env.NODE_ENV == 'production' ? true : false;
+  constructor(routes: Route[]) {
+    this.app = express();
+    this.port = process.env.PORT || 5000;
+    this.production = process.env.NODE_ENV == "production" ? true : false;
 
-        this.connectToDatabase();
-        this.initializeMiddleware();
-        this.initializerRoutes(routes);
-        this.initializeErrorMiddleware();
-    }
-    public listen(){
-        this.app.listen(this.port, () => {
-            Logger.info(`Server is listening on port ${this.port}`);
-        })
-    }
-    private initializerRoutes(routes: Route[]){
-        routes.forEach((route) => {
-            this.app.use('/', route.router);
-        });
-    }
+    this.connectToDatabase();
+    this.initializeMiddleware();
+    this.initializerRoutes(routes);
+    this.initializeErrorMiddleware();
+    this.initializeSwagger();
+  }
+  public listen() {
+    this.app.listen(this.port, () => {
+      Logger.info(`Server is listening on port ${this.port}`);
+    });
+  }
+  private initializerRoutes(routes: Route[]) {
+    routes.forEach((route) => {
+      this.app.use("/", route.router);
+    });
+  }
 
-    private initializeMiddleware(){
-        if (this.production) {
-            this.app.use(hpp());
-            this.app.use(helmet());
-            this.app.use(morgan('combined'));
-            this.app.use(cors({ origin: 'your.domain.com', credentials: true }));
-          } else {
-            this.app.use(morgan('dev'));
-            this.app.use(cors({ origin: true, credentials: true }));
-          }
-          this.app.use(express.json());
-          this.app.use(express.urlencoded({ extended: true }));
+  private initializeMiddleware() {
+    if (this.production) {
+      this.app.use(hpp());
+      this.app.use(helmet());
+      this.app.use(morgan("combined"));
+      this.app.use(cors({ origin: "your.domain.com", credentials: true }));
+    } else {
+      this.app.use(morgan("dev"));
+      this.app.use(cors({ origin: true, credentials: true }));
     }
-    private initializeErrorMiddleware() {
-      this.app.use(errorMiddleware);
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+  }
+  private initializeErrorMiddleware() {
+    this.app.use(errorMiddleware);
+  }
+  private connectToDatabase() {
+    const connectString = process.env.MONGODB_URI;
+    if (!connectString) {
+      Logger.error("Connection string is invalid");
+      return;
     }
-    private connectToDatabase(){
-        const connectString = process.env.MONGODB_URI;
-        if (!connectString) {
-          Logger.error('Connection string is invalid');
-          return;
-        }
-        //const connectString = 'mongodb+srv://dungpv:123456a@@master.jw6k2.mongodb.net/dungpv_social?retryWrites=true&w=majority';
-        mongoose
-          .connect(connectString, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true,
-          })
-          .catch((reason) => {
-            Logger.error(reason);
-          });
-        Logger.info('Database connected...');
-      }
+    //const connectString = 'mongodb+srv://dungpv:123456a@@master.jw6k2.mongodb.net/dungpv_social?retryWrites=true&w=majority';
+    mongoose
+      .connect(connectString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+      })
+      .catch((reason) => {
+        Logger.error(reason);
+      });
+    Logger.info("Database connected...");
+  }
 
+  private initializeSwagger() {
+    const swaggerDocument = YAML.load("./src/swagger.yaml");
+
+    this.app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  }
 }
 
 export default App;
--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -1,10 +1,10 @@
-import AuthController from './auth.controller';
-import { Route } from '@core/interfaces';
-import { Router } from 'express';
-import { authMiddleware } from '@core/middleware';
+import AuthController from "./auth.controller";
+import { Route } from "@core/interfaces";
+import { Router } from "express";
+import { authMiddleware } from "@core/middleware";
 
 export default class AuthRoute implements Route {
-  public path = '/api/auth';
+  public path = "/api/v1/auth";
   public router = Router();
 
   public authController = new AuthController();
@@ -15,6 +15,10 @@ export default class AuthRoute implements Route {
 
   private initializeRoutes() {
     this.router.post(this.path, this.authController.login); //POST: http://localhost:5000/api/auth
-    this.router.get(this.path, authMiddleware, this.authController.getCurrentLoginUser); //GET: http://localhost:5000/api/auth --> Require login
+    this.router.get(
+      this.path,
+      authMiddleware,
+      this.authController.getCurrentLoginUser
+    ); //GET: http://localhost:5000/api/auth --> Require login
   }
 }
